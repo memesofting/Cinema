8c8db79bec371d0ca090f90fbb01ee7db6fcd916
memesofting <bibitayoadekunle00@gmail.com>
2024-05-09 22:38:15 +0100

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Cinema.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinema.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
