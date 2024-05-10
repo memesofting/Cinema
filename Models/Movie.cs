23e07a2e9c994a4ab6ae96a7faeb8dc4e70ed0af
memesofting <bibitayoadekunle00@gmail.com>
2024-05-10 10:36:27 +0100

    Database created

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -3,12 +3,12 @@
     public class Movie
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Genre { get; set; }
-        public string Description { get; set; }
-        public string Duration { get; set; }
+        public string? Title { get; set; }
+        public string? Genre { get; set; }
+        public string? Description { get; set; }
+        public string? Duration { get; set; }
         public TimeOnly ShowTime { get; set; }
         public DateOnly ShowDate { get; set; }
-        public string Location { get; set; }
+        public string? Location { get; set; }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Cinema
+# migration name: AddMovieToDatabase
