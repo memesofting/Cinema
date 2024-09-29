b34521aad3a32874847dedce0edeb26df8385f40
Ademicope <84354061+Ademicope@users.noreply.github.com>
2024-09-29 23:34:38 +0100

    Linked movies with database for home view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,17 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Cinema.Data;
+using Cinema.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Cinema.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
         private readonly ILogger<HomeController> _logger;
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext db, ILogger<HomeController> logger)
         {
+            _db = db;
             _logger = logger;
         }
+
+        
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Movie> objMovieList = _db.Movies;
+            return View(objMovieList);
         }
     }
 }
